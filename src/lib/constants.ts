17b963e66983116e1a25d1a770ded9ab9e081b66
PhatDb <mrkujitv@gmail.com>
2025-04-12 15:28:46 +0700

    02-create-layout

---
+++ src/lib/constants.ts
@@ -0,0 +1,7 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'TechParadise'
+export const APP_SLOGAN =
+  process.env.NEXT_PUBLIC_APP_SLOGAN ||
+  'Thiên đường linh kiện – Đỉnh cao hiệu năng'
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  'Nơi bạn dễ dàng tìm thấy mọi linh kiện máy tính từ phổ thông đến cao cấp, với dịch vụ chuyên nghiệp, hỗ trợ nhiệt tình và giá cả hợp lý. TechParadise – đồng hành cùng mọi bản build của bạn.'
