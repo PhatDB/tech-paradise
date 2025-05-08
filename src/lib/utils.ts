395f4699781f9adea1f030b5378e5ed66e17d78a
PhatDb <mrkujitv@gmail.com>
2025-05-08 21:25:17 +0700

    update-source

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,26 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import {clsx, type ClassValue} from "clsx"
+import {twMerge} from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs))
+}
+
+export interface ApiError extends Error {
+    status?: number;
+}
+
+export function formatError(error: unknown): ApiError {
+    const fallback = new Error('Internal server error') as ApiError;
+    fallback.status = 500;
+
+    if (error instanceof Error) {
+        const apiError = error as ApiError;
+        return {
+            name: apiError.name,
+            message: apiError.message,
+            status: apiError.status ?? 500,
+        };
+    }
+
+    return fallback;
 }
